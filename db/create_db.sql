044f9e7de97de935f2a58c4611412e720575d19c
Gabriel Oliveira Galvao do Vale <Gabriel.Oliveira.Galvao.do.Vale@ibm.com>
2025-07-12 01:29:56 -0600

    Remove default 0

--- db/create_db.sql
+++ db/create_db.sql
@@ -3,6 +3,9 @@
 CREATE TABLE IoC (
 	ioc TEXT NOT NULL,
 	ioc_type TEXT NOT NULL,
-	vt_detections INTEGER DEFAULT (0), created_at TEXT, updated_at TEXT, last_seen TEXT,
+	vt_detections INTEGER,
+	created_at TEXT,
+	updated_at TEXT,
+	last_seen TEXT,
 	CONSTRAINT IoC_PK PRIMARY KEY (ioc)
 );
